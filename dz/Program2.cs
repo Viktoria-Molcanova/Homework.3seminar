43564ca1da54119fc0e804119a2bcb9f1c8046f0
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-09 23:57:00 +0700

    create program2

--- dz/Program2.cs
+++ dz/Program2.cs
@@ -1,5 +1,22 @@
-Console.WriteLine("Введите массив");
-int[] A = new int [5];
-i = 0;
-if index[0]
-Console.WriteLine(1);
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int a=n;
+int b =0;
+int sum = 0;
+while ( n > 0)
+{
+b = n % 10;
+sum = (sum * 10) + b;
+n = n / 10; 
+
+}
+if( a == sum )
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+
