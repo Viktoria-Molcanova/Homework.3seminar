2de0f1a55b92b049cdce1482ef07ff2010440eb9
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-09 23:59:21 +0700

    update program3

--- dz/Program3.cs
+++ dz/Program3.cs
@@ -1,7 +1,7 @@
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-Console.WriteLine("Введите число ");
+Console.WriteLine(" Введите число ");
 int a = Convert.ToInt32(Console.ReadLine());
 for (int b = 1; b <= a; b++)
 { 
-    Console.Write($"{b*b*b} ");
+    Console.Write($"{ b * b * b } ");
 }
